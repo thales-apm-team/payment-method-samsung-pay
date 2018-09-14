01922225c8eff21008e618dbde0888328a8649be
Anthony giordana <brice.junoy@mythalesgroup.io>
2018-09-14 13:57:21 +0200

    test chiffrement / déchiffrement

--- src/main/java/com/payline/payment/samsung/pay/utils/JweDecrypt.java
+++ src/main/java/com/payline/payment/samsung/pay/utils/JweDecrypt.java
@@ -4,6 +4,9 @@ import javax.crypto.*;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -11,19 +14,14 @@ import java.util.Base64;
 
 public class JweDecrypt {
 
-    public static final String RSA = "RSA";
-    public static final String AES = "AES";
-    public static final String AES_GCM_NO_PADDING = "AES/GCM/NoPadding";
-    public static final String RSA_ECB_PKCS_1_PADDING = "RSA/ECB/PKCS1Padding";
-    public static final String DELIMS = "[.]";
+    private static final String RSA = "RSA";
+    private static final String AES = "AES";
+    private static final String AES_GCM_NO_PADDING = "AES/GCM/NoPadding";
+    private static final String RSA_ECB_PKCS_1_PADDING = "RSA/ECB/PKCS1Padding";
+    private static final String DELIMS = "[.]";
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        String encPayload = "eyJhbGciOiJSU0ExXzUiLCJraWQiOiJPK0YwcGFQZGZRT005UXhLSDBhd0ZUSTVpWVFETXY4Q0FDWlZRT24xeXBVPSIsInR5cCI6IkpPU0UiLCJjaGFubmVsU2VjdXJpdHlDb250ZXh0IjoiUlNBX1BLSSIsImVuYyI6IkExMjhHQ00ifQ.xmDaYGMyCDxfLcwT0q_xgnxMTsmeT0ZYQ7v04vdQG8_jliyXQA0DJZJqSrysxhKBSutn-CxLgFzrZ4w2WPlgu6sNcTaM42A1y1bjyPcwhHP_RfTPdZX1xlZi_huROHK4KVa9FhOug_1nUMcOJHJTbupUNWUlG-OFbdEAXNTrYAm-EVkKjo0_ZOxTsb2EZpKW94trJl-GGTwwOzx1Gib9JMPFvYlEH00qUdGDW_pTwUExuV3TDEgnrmUb6-rpMBlVqTJLsMKwrDnV5srAr2aQPzyvhNP8d1qJvORG2-g-IzfH91H9XhgSd6nhf0XqaCNE22ulQgKf5vFSSZjzCO-Whw.fFzZYk91o87S72k5.MbsuTH989WtsqThKpcPn8cB41QSp2prdmZlk12wz10a061WT25wm9Euwe4JXke_A04tarEFaIuQV-uhgWrfYFv3mDC9kbBSio7RjjxS5_PkvXURAcE0-OMLhjnHW7E-6K75A8IlsAsCgTTOsmTn90E7oTTEA3Kgqm13mWQANPMNXlcSwkdHI7ViMD4L7OGzXUP2zpwx5XrjhyDS5ybIdxG8XpusiEpkq9Qdzx_8dc9KH2pRrIQZxvtk.Ly-8HAjFp_3BkUeuRomqjA";
-        String privateKeyFilePath = "./rsapriv.der";
-        getDecryptedData(encPayload, privateKeyFilePath);
+    public JweDecrypt() {
+        // ras.
     }
 
     /**
@@ -31,7 +29,7 @@ public class JweDecrypt {
      * @param privateKeyFilePath private key path.
      * @return Decrypted text.
      */
-    public static String getDecryptedData(String encPayload, String privateKeyFilePath) {
+    public String getDecryptedData(String encPayload, String privateKeyFilePath) throws Exception {
         String[] tokens = encPayload.split(DELIMS);
         Base64.Decoder urlDecoder = Base64.getUrlDecoder();
         byte[] encKey = urlDecoder.decode(tokens[1]);
@@ -39,18 +37,26 @@ public class JweDecrypt {
         byte[] cipherText = urlDecoder.decode(tokens[3]);
         byte[] tag = urlDecoder.decode(tokens[4]);
         byte[] plainText = new byte[cipherText.length];
-        try {
             // Read private key file
-            File privateKeyFile = new File(privateKeyFilePath);
-            DataInputStream dis = new DataInputStream(new FileInputStream(privateKeyFile));
-            byte[] privKeyBytes = new byte[(int) privateKeyFile.length()];
-            dis.read(privKeyBytes);
-            dis.close();
+//            File privateKeyFile = new File(privateKeyFilePath);
+
+            ClassLoader classLoader = JweDecrypt.class.getClassLoader();
+            File keyFile = new File(classLoader.getResource(privateKeyFilePath).getFile());
+//            DataInputStream dis = new DataInputStream(new FileInputStream(keyFile));
+
+
+        Path path = Paths.get(keyFile.getPath());
+        byte[] privKeyBytes = Files.readAllBytes(path);
+
+
+//        byte[] privKeyBytes = new byte[(int) keyFile.length()];
+//            dis.read(privKeyBytes);
+//            dis.close();
             // Set private key spec
             PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(privKeyBytes);
             KeyFactory keyFactory = KeyFactory.getInstance(RSA);
-            PrivateKey privKey = keyFactory.generatePrivate(spec);
             Cipher decryptCipher = Cipher.getInstance(RSA_ECB_PKCS_1_PADDING);
+            PrivateKey privKey = keyFactory.generatePrivate(spec);
             decryptCipher.init(Cipher.DECRYPT_MODE, privKey);
             byte[] plainEncKey = decryptCipher.doFinal(encKey);
             final Cipher aes128Cipher = Cipher.getInstance(AES_GCM_NO_PADDING);
@@ -60,11 +66,7 @@ public class JweDecrypt {
             int offset = aes128Cipher.update(cipherText, 0, cipherText.length, plainText, 0);
             aes128Cipher.update(tag, 0, tag.length, plainText, offset);
             aes128Cipher.doFinal(plainText, offset);
-        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException
-                | InvalidKeyException | IllegalBlockSizeException | BadPaddingException
-                | InvalidAlgorithmParameterException | ShortBufferException e) {
-        }
+
         return new String(plainText);
     }
-
 }
---
+++ src/test/java/com/payline/payment/samsung/pay/utils/JweDecryptTest.java
@@ -0,0 +1,67 @@
+package com.payline.payment.samsung.pay.utils;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
+import javax.crypto.Cipher;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+
+public class JweDecryptTest {
+
+//    private String encPayload =
+//            "eyJhbGciOiJSU0ExXzUiLCJraWQiOiJPK0YwcGFQZGZRT005UXhLSDBhd0ZUSTVpWVFETXY4Q0FDWlZRT24xeXBVPSIsInR5cCI6I" +
+//            "kpPU0UiLCJjaGFubmVsU2VjdXJpdHlDb250ZXh0IjoiUlNBX1BLSSIsImVuYyI6IkExMjhHQ00ifQ.xmDaYGMyCDxfLcwT0q_xgnxM" +
+//            "TsmeT0ZYQ7v04vdQG8_jliyXQA0DJZJqSrysxhKBSutn-" +
+//            "CxLgFzrZ4w2WPlgu6sNcTaM42A1y1bjyPcwhHP_RfTPdZX1xlZi_huROHK4KVa9FhOug_1nUMcOJHJTbupUNWUlG-" +
+//            "OFbdEAXNTrYAm-EVkKjo0_ZOxTsb2EZpKW94trJl-GGTwwOzx1Gib9JMPFvYlEH00qUdGDW_pTwUExuV3TDEgnrmUb6-" +
+//            "rpMBlVqTJLsMKwrDnV5srAr2aQPzyvhNP8d1qJvORG2-g-IzfH91H9XhgSd6nhf0XqaCNE22ulQgKf5vFSSZjzCO-" +
+//            "Whw.fFzZYk91o87S72k5.MbsuTH989WtsqThKpcPn8cB41QSp2prdmZlk12wz10a061WT25wm9Euwe4JXke_A04tarEFaIuQV-" +
+//            "uhgWrfYFv3mDC9kbBSio7RjjxS5_PkvXURAcE0-OMLhjnHW7E-" +
+//            "6K75A8IlsAsCgTTOsmTn90E7oTTEA3Kgqm13mWQANPMNXlcSwkdHI7ViMD4L7OGzXUP2zpwx5XrjhyDS5ybIdxG8XpusiEpkq9Qdzx" +
+//            "_8dc9KH2pRrIQZxvtk.Ly-8HAjFp_3BkUeuRomqjA";
+
+    private String encPayload =
+            "eeyJhbGciOiJSU0ExXzUiLCJraWQiOiJPK0YwcGFQZGZRT005UXhLSDBhd0ZUSTVpWVFETXY4Q0FDWlZRT24xeXBVPSIsInR5cCI6IkpPU0UiLCJjaGFubmVsU2VjdXJpdHlDb250ZXh0IjoiUlNBX1BLSSIsImVuYyI6IkExMjhHQ00ifQ.xmDaYGMyCDxfLcwT0q_xgnxMTsmeT0ZYQ7v04vdQG8_jliyXQA0DJZJqSrysxhKBSutn-CxLgFzrZ4w2WPlgu6sNcTaM42A1y1bjyPcwhHP_RfTPdZX1xlZi_huROHK4KVa9FhOug_1nUMcOJHJTbupUNWUlG-OFbdEAXNTrYAm-EVkKjo0_ZOxTsb2EZpKW94trJl-GGTwwOzx1Gib9JMPFvYlEH00qUdGDW_pTwUExuV3TDEgnrmUb6-rpMBlVqTJLsMKwrDnV5srAr2aQPzyvhNP8d1qJvORG2-g-IzfH91H9XhgSd6nhf0XqaCNE22ulQgKf5vFSSZjzCO-Whw.fFzZYk91o87S72k5.MbsuTH989WtsqThKpcPn8cB41QSp2prdmZlk12wz10a061WT25wm9Euwe4JXke_A04tarEFaIuQV-uhgWrfYFv3mDC9kbBSio7RjjxS5_PkvXURAcE0-OMLhjnHW7E-6K75A8IlsAsCgTTOsmTn90E7oTTEA3Kgqm13mWQANPMNXlcSwkdHI7ViMD4L7OGzXUP2zpwx5XrjhyDS5ybIdxG8XpusiEpkq9Qdzx_8dc9KH2pRrIQZxvtk.Ly-8HAjFp_3BkUeuRomqjA";
+
+    private String privateKeyFilePath = "keystore/rsapriv.der";
+//private String privateKeyFilePath = "D:/Brice/PROJET_PAYLINE/payment-method-samsung-pay/src/test/resources/keystore/rsapriv.der";
+    private JweDecrypt service;
+
+    @Before
+    public void setUp(){
+        // ras.
+        service = new  JweDecrypt();
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void getDecryptedData() throws Exception {
+        String res = service.getDecryptedData(encPayload, privateKeyFilePath);
+        System.out.print(res);
+        Assert.assertEquals(true, true);
+
+    }
+
+    @Test
+    public void testPadding() throws Exception {
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(1024, random);
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        /* constant 117 is a public key size - 11 */
+        byte[] plaintext = new byte[117];
+        random.nextBytes(plaintext);
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+        byte[] ciphertext = cipher.doFinal(plaintext);
+        System.out.println(plaintext.length + " becomes " + ciphertext.length);
+    }
+}
